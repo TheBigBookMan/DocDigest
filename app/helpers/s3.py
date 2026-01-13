a5b7d0523a6a80352dfa0ed03d41bc9ca1cd3c1e
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-13 16:15:50 +1030

    check for bucket exists in S3

--- app/helpers/s3.py
+++ app/helpers/s3.py
@@ -3,9 +3,19 @@ import boto3
 s3 = boto3.client('s3')
 
 def upload_file_to_s3(file_path, bucket, key):
+
+
     try:
         s3.upload_fileobj(file_path, bucket, key)
         return True
 
     except Exception as e:
-        return f"Could not upload: {file_path}. Error: {e.message}"
+        return f"Could not upload: {file_path}. Error: {e}"
+
+def check_bucket_exists(bucket_name):
+    try:
+        s3.head_bucket(Bucket=bucket_name)
+        return True
+
+    except Exception as e:
+        return False
