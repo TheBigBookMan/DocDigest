a3150e06fe5b99287a4cf073020f41527986ba61
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-15 09:35:58 +1030

    create Config class for modularity

---
+++ app/config.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    ENVIRONMENT = os.getenv('ENVIRONMENT')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION')
+    S3_BUCKET = os.getenv('S3_BUCKET')
+    DYNAMO_DB_TABLE = os.getenv('DYNAMO_DB_TABLE')
+
+
