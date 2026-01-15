2c0331c645248d419cecfa4f06d16d5a396049eb
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-15 11:27:28 +1030

    refactor s3 handling to be class rather than functional

--- app/app.py
+++ app/app.py
@@ -3,11 +3,10 @@ import uuid
 import datetime
 
 from flask import Flask, render_template, request
-from helpers import functions, s3, dynamo_db
-
 from utils import logs
-from components import file_validation, upload_to_s3
+from components import file_validation
 from services import S3Service
+import config
 
 app = Flask(__name__)
 
@@ -44,8 +43,11 @@ def upload():
 
     files_to_process = validate_files['files']
 
+    config_handler = config.Config()
+    s3_handler = S3Service(config_handler.S3_BUCKET, config_handler.AWS_DEFAULT_REGION)
+
     # Handle uploading files to S3 bucket
-    handle_s3 = upload_to_s3.upload_files_to_s3(files_to_process, session_id)
+    handle_s3 = s3_handler.insert_s3(session_id, files_to_process)
 
     if handle_s3['status'] == 'error':
         return handle_s3
@@ -54,32 +56,32 @@ def upload():
 
 
 
-    table_name = os.getenv('DYNAMO_DB_TABLE')
-    table_exists = dynamo_db.check_table_exists(table_name)
-
-    if not table_exists:
-        return {
-            'status': 'error',
-            'message': 'DynamoDB table does not exist'
-        }
-
-    payload = {
-        'session_id': session_id,
-        'email': email,
-        'total_files': len(uploaded_files),
-        'completed_files': '0',
-        'uploaded_time': datetime.now(),
-        'response': []
-    }
-
-    insert_dynamo = dynamo_db.create_item(table_name, payload)
-
-    if not insert_dynamo:
-        return {
-            'status': 'error',
-            'message': 'DynamoDB error'
-        }
-
+    # table_name = os.getenv('DYNAMO_DB_TABLE')
+    # table_exists = dynamo_db.check_table_exists(table_name)
+    #
+    # if not table_exists:
+    #     return {
+    #         'status': 'error',
+    #         'message': 'DynamoDB table does not exist'
+    #     }
+    #
+    # payload = {
+    #     'session_id': session_id,
+    #     'email': email,
+    #     'total_files': len(uploaded_files),
+    #     'completed_files': '0',
+    #     'uploaded_time': datetime.now(),
+    #     'response': []
+    # }
+    #
+    # insert_dynamo = dynamo_db.create_item(table_name, payload)
+    #
+    # if not insert_dynamo:
+    #     return {
+    #         'status': 'error',
+    #         'message': 'DynamoDB error'
+    #     }
+    #
 
 
     return {
--- app/services/s3_service.py
+++ app/services/s3_service.py
@@ -1,4 +1,56 @@
+import boto3
+from utils import logs
 
 class S3Service:
-    def __init__(self):
-        print("init S3Servcice")
+    def __init__(self, bucket_name, s3_region):
+        self.bucket_name = bucket_name.lower()
+        self.client = boto3.client('s3', region_name=s3_region)
+        self.logger = logs.get_logger('s3')
+
+    def check_bucket_exists(self):
+        self.logger.info(f"Checking if bucket {self.bucket_name} exists")
+
+        try:
+            self.client.head_bucket(Bucket=self.bucket_name)
+            self.logger.info(f"Bucket {self.bucket_name} exists")
+            return True
+
+        except Exception as e:
+            self.logger.error(e)
+            return False
+
+    def insert_s3(self, session_id, files):
+        bucket_exists = self.check_bucket_exists()
+
+        if not bucket_exists:
+            self.logger.info(f"Bucket {self.bucket_name} does not exist")
+            return {
+                'status': 'error',
+                'message': 'S3 bucket does not exist'
+            }
+
+        uploaded_files = []
+
+        for file in files:
+            upload_response = self.upload_file(file, f"{session_id}/{file.filename}")
+
+            if upload_response:
+                uploaded_files.append(file.filename)
+
+
+        return {
+            'status': 'success',
+            'uploaded_files': uploaded_files
+        }
+
+    def upload_file(self, file, key):
+        self.logger.info(f"Uploading to S3 bucket {self.bucket_name} and key {key}")
+
+        try:
+            self.client.upload_fileobj(file, self.bucket_name, key)
+            self.logger.info(f"Successfully uploaded to S3 bucket {self.bucket_name} and key {key}")
+            return True
+
+        except Exception as e:
+            self.logger.error(e)
+            return False
