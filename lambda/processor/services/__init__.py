1e0f8b968f748a69cb22de8092c2d4d1ed3c523f
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-16 11:41:24 +1030

    easier modular structure for services

---
+++ lambda/processor/services/__init__.py
@@ -0,0 +1,9 @@
+from .s3_service import S3Service
+# from .sns_service import SNSService
+from .claude_service import ClaudeService
+
+__all__ = [
+    'S3Service',
+    # 'SNSService',
+    'ClaudeService'
+]
