39191a1f32fb1b87f9ca5efceefe8b81e9569a35
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-22 13:24:33 +1030

    fix up python set bug in dynamo

--- app/app.py
+++ app/app.py
@@ -68,7 +68,7 @@ def upload():
         'email': email,
         'total_files': len(uploaded_files),
         'completed_count': 0,
-        'processed_files': set(),
+        'processed_files': {'__INIT__'},
         'status': 'PROCESSING',
         'uploaded_at': datetime.datetime.now().isoformat(),
         'completed_at': None,
