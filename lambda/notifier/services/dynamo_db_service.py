dbebc43588e41398bba8f577b809486b8615588a
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-27 18:53:19 +1030

    create get_item method on DynamoDBService object

--- lambda/notifier/services/dynamo_db_service.py
+++ lambda/notifier/services/dynamo_db_service.py
@@ -7,3 +7,14 @@ class DynamoDBService:
         self.client = boto3.client('dynamodb', region_name=region_name)
         self.table_name = table_name
 
+    def get_item(self, session_id):
+        self.logger.info(f"Getting item for session_id: {session_id}")
+
+        try:
+            return self.client.get_item(
+                TableName=self.table_name,
+                Key={session_id: {'S': session_id}}
+            )
+        except Exception as e:
+            self.logger.error(e)
+            return False
