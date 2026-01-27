65dd6411d62ec5150a82e240402b7f4f82eef42d
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-27 18:51:31 +1030

    notifier lambda create dynamo db service

--- lambda/notifier/services/dynamo_db_service.py
+++ lambda/notifier/services/dynamo_db_service.py
@@ -1,4 +1,9 @@
-
+from utils import logs
+import boto3
 
 class DynamoDBService:
-    ...
+    def __init__(self, region_name, table_name):
+        self.logger = logs.get_logger('dynamodb')
+        self.client = boto3.client('dynamodb', region_name=region_name)
+        self.table_name = table_name
+
