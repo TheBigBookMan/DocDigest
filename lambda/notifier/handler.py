374c1b6eb1dbf546ea237b4edd6aadadd577497a
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-27 13:17:44 +1030

    add in mock lambda notifier invocation and parse SNS event function

---
+++ lambda/notifier/handler.py
@@ -0,0 +1,53 @@
+import boto3
+import config
+from utils import logs
+from helper import parse_lambda
+from services import DynamoDBService, SNSService
+
+logger = logs.get_logger('lambda-notifier')
+
+def lambda_handler(event, context):
+    logger.info("Starting lambda-notifier handler")
+
+    # Get event information
+    lambda_event_data = parse_lambda(event)
+
+    print(lambda_event_data)
+
+
+if __name__ == '__main__':
+    print("Testing lambda notiofier")
+
+    event = {
+      "Records": [
+        {
+          "EventVersion": "1.0",
+          "EventSubscriptionArn": "arn:aws:sns:us-east-1:123456789012:sns-lambda:21be56ed-a058-49f5-8c98-aedd2564c486",
+          "EventSource": "aws:sns",
+          "Sns": {
+            "SignatureVersion": "1",
+            "Timestamp": "2019-01-02T12:45:07.000Z",
+            "Signature": "tcc6faL2yUC6dgZdmrwh1Y4cGa/ebXEkAi6RibDsvpi+tE/1+82j...65r==",
+            "SigningCertURL": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-ac565b8b1a6c5d002d285f9598aa1d9b.pem",
+            "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+            "Message": "{\"event_type\":\"session_complete\",\"session_id\":\"abc123\",\"email\":\"user@example.com\",\"total_files\":3,\"completed_at\":\"2026-01-22T14:30:45.123Z\",\"uploaded_at\":\"2026-01-22T14:29:30.000Z\"}",
+            "MessageAttributes": {
+              "Test": {
+                "Type": "String",
+                "Value": "TestString"
+              },
+              "TestBinary": {
+                "Type": "Binary",
+                "Value": "TestBinary"
+              }
+            },
+            "Type": "Notification",
+            "UnsubscribeUrl": "https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&amp;SubscriptionArn=arn:aws:sns:us-east-1:123456789012:test-lambda:21be56ed-a058-49f5-8c98-aedd2564c486",
+            "TopicArn":"arn:aws:sns:us-east-1:123456789012:sns-lambda",
+            "Subject": "TestInvoke"
+          }
+        }
+      ]
+    }
+    context = {}
+    lambda_handler(event, context)
--- lambda/notifier/helper.py
+++ lambda/notifier/helper.py
@@ -1,13 +1,12 @@
 import json
 
 def parse_lambda(event):
-    event_body = event['Records'][0]['body']
-    s3_event = json.loads(event_body)
-    event_records = s3_event['Records'][0]
+    event_body = event['Records'][0]
+    sns_event = event_body['Sns']
 
     return {
-        'event_source': event_records['eventSource'],
-        'event_time': event_records['eventTime'],
-        'event_name': event_records['eventName'],
-        's3': event_records['s3'],
+        'event_source': event_body['EventSource'],
+        'event_time': sns_event['Timestamp'],
+        'event_type': sns_event['Type'],
+        'event_message': sns_event['Message'],
     }
