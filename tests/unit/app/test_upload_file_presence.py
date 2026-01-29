a67e601c8cb32ffefd7040a09b4132205e07b2d1
Benjamin Smerd <bjsmerd@gmail.com>
2026-01-29 21:17:27 +1030

    add in unit test for error return if files uploaded dont pass validation

--- tests/unit/app/test_upload_file_presence.py
+++ tests/unit/app/test_upload_file_presence.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 
 class TestUploadFilePresence:
     """Unit test for the file validation"""
@@ -14,3 +15,25 @@ class TestUploadFilePresence:
         assert json_data['status'] == 'error'
         assert 'Files need to be uploaded' in json_data['message']
 
+    @patch('app.file_validation.validate')
+    def test_validation_error_returned(self, mock_validation, client, sample_file):
+        """If file field passed in POST request but then fails validate(), endpoint return error message"""
+
+        mock_validation.return_value = {
+            'status': 'error',
+            'message': 'Files need to be uploaded'
+        }
+
+        response = client.post('/upload',
+            data={
+                'email': 'test@gmail.com',
+                'files': [sample_file]
+            },
+            content_type='multipart/form-data'
+        )
+
+        json_data = response.get_json()
+        assert json_data['status'] == 'error'
+        assert response.status_code == 400
+
+        mock_validation.assert_called_once()
