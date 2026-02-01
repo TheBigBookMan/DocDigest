09cf9cb52f0d5ff42a8ce64b36bc4a0a8ca9c560
Benjamin Smerd <bjsmerd@gmail.com>
2026-02-01 16:56:55 +1030

    make the webapp docker image tag with the git hash and latest

--- scripts/deploy_webapp.sh
+++ scripts/deploy_webapp.sh
@@ -5,17 +5,23 @@ if [ -f ../app/.env ]; then
   export $(grep -v '^#' ../app/.env | xargs)
 fi
 
+GIT_SHA=$(git rev-parse --short HEAD)
+ECR_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+
 echo "Starting retrieval of ECR authentication"
-aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_URL}
 echo "Successfully retrieved ECR authentication"
 
 echo "Starting docker image build for webapp"
-docker build -t ${ECR_REPO_NAME} ..
+docker build -t ${ECR_REPO_NAME}:${GIT_SHA} ..
 echo "Finished building webapp image"
 
 echo "Tagging image"
-docker tag ${ECR_REPO_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest
+docker tag ${ECR_REPO_NAME}:${GIT_SHA} ${ECR_REPO_NAME}:latest
+docker tag ${ECR_REPO_NAME}:${GIT_SHA} ${ECR_URL}/${ECR_REPO_NAME}:${GIT_SHA}
+docker tag ${ECR_REPO_NAME}:${GIT_SHA} ${ECR_URL}/${ECR_REPO_NAME}:latest
 
 echo "Pushing docker image to ECR"
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest
+docker push ${ECR_URL}/${ECR_REPO_NAME}:latest
+docker push ${ECR_URL}/${ECR_REPO_NAME}:${GIT_SHA}
 echo "Successfully pushed docker image to ECR"
